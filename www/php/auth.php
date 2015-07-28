4622f2189dd95aefba44e4863e2cdbfd81ca12ec
Yue Zhao <jzhao156@gmail.com>
2015-07-28 16:51:41 -0700

    Made callback file after auth calls linkedin get request

--- www/php/auth.php
+++ www/php/auth.php
@@ -1,5 +1,10 @@
 <?php
 
-    echo "It's working";
+    $response = http_get("https://www.linkedin.com/uas/oauth2/authorization?" +
+    "response_type=code&" +
+    "client_id=75d2ob10meoc3a&" +
+    "redirect_uri=http://kawaiikrew.net/www/php/authcallback.php&" +
+    "state=987654321" +
+    "&scope=r_basicprofile", array("timeout"=>1), $info);
 
 ?>
