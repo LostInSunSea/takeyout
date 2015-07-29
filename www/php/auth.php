c81bbba52f7226fa5d7b518892e7fee3797f5730
Yue Zhao <jzhao156@gmail.com>
2015-07-28 17:04:21 -0700

    Example aye

--- www/php/auth.php
+++ www/php/auth.php
@@ -1,7 +1,10 @@
 <?php
 
-    $response = http_get("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http://kawaiikrew.net/www/php/authcallback.php&state=987654321&scope=r_basicprofile", array("timeout"=>1), $info);
+    //$response = http_get("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http://kawaiikrew.net/www/php/authcallback.php&state=987654321&scope=r_basicprofile", array("timeout"=>1), $info);
 
-    echo $info;
+    //echo $info;
+    //print_r($info);
+
+    $response = http_get("http://www.example.com/", array("timeout"=>1), $info);
     print_r($info);
 ?>
