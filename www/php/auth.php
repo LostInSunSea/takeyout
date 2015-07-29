ab0fc8b200a45f5404211455640088414316c555
Yue Zhao <jzhao156@gmail.com>
2015-07-29 11:17:27 -0700

    Forgot the state parameter

--- www/php/auth.php
+++ www/php/auth.php
@@ -1,5 +1,5 @@
 <?php
 
-header('https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http%3A%2F%2Fkawaiikrew.net%2Fwww%2Fphp%2Fauthcallback.php');
+header('https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http%3A%2F%2Fkawaiikrew.net%2Fwww%2Fphp%2Fauthcallback.php&state=987654321&scope=r_basicprofile');
 
 ?>
