d0e02de11643e93999d4d8873417e72c21822dae
Yue Zhao <jzhao156@gmail.com>
2015-07-29 13:05:17 -0700

    Added post to the callback to get token

--- www/php/authcallback.php
+++ www/php/authcallback.php
@@ -1,19 +1,19 @@
 <?php
 
 parse_str($_SERVER['QUERY_STRING']);
-echo $code;
-echo $state;
 
-/*$fields = array(
+$fields = array(
   'Host' => 'www.linkedin.com',
   'Content-Type' => 'application/x-www-form-urlencoded'
 );
 $files = array();
 $response = http_post_fields("https://www.linkedin.com/uas/oauth2/accessToken?" +
                              "grant_type=authorization_code&" +
-                             "code=987654321&" +
-                             "redirect_uri=https%3A%2F%2Fwww.myapp.com%2Fauth%2Flinkedin&" +
-                             "client_id=123456789" +
-                             "&client_secret=shhdonottell", $fields, $files);*/
+                             "code=" + $code + "&" +
+                             "redirect_uri="+ "http%3A%2F%2Fkawaiikrew.net%2Fwww%2Fphp%2Fauthcallback.php" + "&" +
+                             "client_id=75d2ob10meoc3a" +
+                             "&client_secret=zQ5SUiuhMRDG0tpk", $fields, $files);
+
+echo $response;
 
 ?>
