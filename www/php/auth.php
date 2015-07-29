21c47e5c272b88bba9f52aa737b53bc70e6f9e9e
Yue Zhao <jzhao156@gmail.com>
2015-07-29 11:10:48 -0700

    Omg all I needed to do was header(...);

--- www/php/auth.php
+++ www/php/auth.php
@@ -1,8 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://static.licdn.com");
-$curl = curl_init('https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http%3A%2F%2Fkawaiikrew.net%2Fwww%2Fphp%2Fauthcallback.php&state=987654321&scope=r_basicprofile');
 
-$resp = curl_exec($curl);
-echo $resp;
+header('https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http%3A%2F%2Fkawaiikrew.net%2Fwww%2Fphp%2Fauthcallback.php');
 
 ?>
