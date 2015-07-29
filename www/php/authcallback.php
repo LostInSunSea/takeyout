f02ae734c7af102a33b0ac371436f60e1b5848ad
Yue Zhao <jzhao156@gmail.com>
2015-07-29 14:18:58 -0700

    Added get request with access token

--- www/php/authcallback.php
+++ www/php/authcallback.php
@@ -2,20 +2,6 @@
 
 parse_str($_SERVER['QUERY_STRING']);
 
-/*$fields = array(
-  'Host' => 'www.linkedin.com',
-  'Content-Type' => 'application/x-www-form-urlencoded'
-);
-$files = array();
-$response = http_post_fields("https://www.linkedin.com/uas/oauth2/accessToken?" +
-                             "grant_type=authorization_code&" +
-                             "code=" + $code + "&" +
-                             "redirect_uri="+ "http%3A%2F%2Fkawaiikrew.net%2Fwww%2Fphp%2Fauthcallback.php" + "&" +
-                             "client_id=75d2ob10meoc3a" +
-                             "&client_secret=zQ5SUiuhMRDG0tpk", $fields, $files);
-
-echo $response;*/
-
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,"https://www.linkedin.com/uas/oauth2/accessToken");
@@ -34,5 +20,10 @@ curl_close ($ch);
 
 $result = json_decode($server_output, true);
 
-echo $result['access_token'];
+$curl = curl_init('https://api.linkedin.com/v1/people/~?oauth2_access_token=' . $result['access_token']);
+
+$result = curl_exec($curl);
+
+echo $result;
+
 ?>
