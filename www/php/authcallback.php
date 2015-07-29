f2c5a8bd4a05d78487544ff70b5b43913b96136a
Yue Zhao <jzhao156@gmail.com>
2015-07-29 11:35:53 -0700

    Parsing querystring for authcallback

--- www/php/authcallback.php
+++ www/php/authcallback.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "ayyyy lmao";
+parse_str($_SERVER['QUERY_STRING']);
+echo $code;
+echo $state;
+
+/*$fields = array(
+  'Host' => 'www.linkedin.com',
+  'Content-Type' => 'application/x-www-form-urlencoded'
+);
+$files = array();
+$response = http_post_fields("https://www.linkedin.com/uas/oauth2/accessToken?" +
+                             "grant_type=authorization_code&" +
+                             "code=987654321&" +
+                             "redirect_uri=https%3A%2F%2Fwww.myapp.com%2Fauth%2Flinkedin&" +
+                             "client_id=123456789" +
+                             "&client_secret=shhdonottell", $fields, $files);*/
 
 ?>
