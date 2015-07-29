771b35e30888f4ca986e4d2119452b51b6264e49
Yue Zhao <jzhao156@gmail.com>
2015-07-28 17:00:48 -0700

    Changed string in auth

--- www/php/auth.php
+++ www/php/auth.php
@@ -1,12 +1,6 @@
 <?php
 
-    $response = http_get("https://www.linkedin.com/uas/oauth2/authorization?" +
-    "response_type=code&" +
-    "client_id=75d2ob10meoc3a&" +
-    "redirect_uri=http://kawaiikrew.net/www/php/authcallback.php&" +
-    "state=987654321" +
-    "&scope=r_basicprofile", array("timeout"=>1), $info);
-
+    $response = http_get("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75d2ob10meoc3a&redirect_uri=http://kawaiikrew.net/www/php/authcallback.php&state=987654321&scope=r_basicprofile", array("timeout"=>1), $info);
 
     echo $info;
 ?>
