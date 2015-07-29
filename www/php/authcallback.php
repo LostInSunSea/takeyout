fcdf164d00459329669baf853ca5b6542f5057c8
Yue Zhao <jzhao156@gmail.com>
2015-07-29 15:19:54 -0700

    Decoded the json

--- www/php/authcallback.php
+++ www/php/authcallback.php
@@ -20,8 +20,12 @@ curl_close ($ch);
 
 $result = json_decode($server_output, true);
 
-$curl = curl_init('https://api.linkedin.com/v1/people/~:(id,location,formatted-name,industry,summary,specialties,positions,headline,picture-urls::(original),interests,languages,skills,date-of-birth)?oauth2_access_token=' . $result['access_token'] . '&format=json');
+$curl = curl_init('https://api.linkedin.com/v1/people/~:(id,location,formatted-name,industry,summary,specialties,positions,headline,picture-urls::(original),picture-url,interests,languages,skills,date-of-birth)?oauth2_access_token=' . $result['access_token'] . '&format=json');
 
 $result = curl_exec($curl);
+
+$decoded = json_decode($result, true);
+
+echo $decoded;
 
 ?>
