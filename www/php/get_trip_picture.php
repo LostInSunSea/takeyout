0ef1bc08e49a3a54a1084f0a8a8af34f8752ae09
Yue Zhao <jzhao156@gmail.com>
2015-08-11 16:26:27 -0700

    test for get trip picture

---
+++ www/php/get_trip_picture.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+//TODO: uncomment this out
+/*if (!isset($_SESSION['id']))
+{
+    exit ("Error: Not logged in!");
+}*/
+
+//TODO: Perform regular expression match to make sure it's actually in the format "city, country"
+
+$locationString = htmlspecialchars($_GET['location']);
+$location = explode(", ", $locationString);
+$city = $location[0];
+$country = $location[1];
+
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "J^mpStrt";
+$dbDatabase = "takeyout";
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM locations WHERE name = '$city' AND type = 'city'";
+
+if ($result = mysqli_query($conn, $sql))
+{
+    if($results->num_rows === 0)
+    {
+        echo "not found";
+    }
+    echo "found";
+}
+
+?>
