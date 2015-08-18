c5af26c7dae45b7febc1058552a612a8dc962966
Yue Zhao <jzhao156@gmail.com>
2015-08-18 16:21:58 -0700

    Rewrote get user data to return more fields

--- www/php/get_user_data.php
+++ www/php/get_user_data.php
@@ -1,7 +1,8 @@
 <?php
 
     session_start();
-    if (!isset($_SESSION['id']))
+    //TODO: uncomment the login check and also decide if I want to use session tokens or do SQL later on
+    /*if (!isset($_SESSION['id']))
     {
         exit ("Error: Not logged in!");
     }
@@ -10,8 +11,37 @@
         'ID' => $_SESSION['id'],
         'Name' => $_SESSION['name'],
         'PicURL' => $_SESSION['picUrl'],
-    );
+    );*/
 
+    //TODO:remove this later
+    $id = 'N-NcRMZupE';
+
+    $dbHost = "localhost";
+    $dbUser = "root";
+    $dbPass = "J^mpStrt";
+    $dbDatabase = "takeyout";
+
+    $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+    if ($conn->connect_error)
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM user WHERE id = '$id'";
+    if ($result=mysqli_query($conn,$sql))
+    {
+        while ($row = mysqli_fetch_array($result))
+        {
+            $user = array(
+                'name' => $row['name'],
+                'headline' => $row['headline'],
+                'city' => $row['city'],
+                'country' => $row['country'],
+                'picFull' => $row['picFull'],
+                'bio' => $row['bio']
+            );
+        }
+    }
     $jsonstring = json_encode($user);
     echo $jsonstring;
 
