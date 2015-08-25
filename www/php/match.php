4ef7b7421249336b1f0fdded677a66092e919bab
Yue Zhao <yuz217@ucsd.edu>
2015-08-25 16:07:37 -0700

    changed messages

--- www/php/match.php
+++ www/php/match.php
@@ -34,7 +34,7 @@
     else
     {
         //TODO: Select users who have a trip here on overlapping days
-        echo '[]';
+        exit ('TODO');
     }
 
     if($result=mysqli_query($conn,$sql))
