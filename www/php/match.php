e8c744c69867a04fcdf65379ff11fa6537deb8be
Yue Zhao <yuz217@ucsd.edu>
2015-08-25 15:31:40 -0700

    testing

--- www/php/match.php
+++ www/php/match.php
@@ -1,13 +1,13 @@
 <?php
 
     session_start();
-    if (!isset($_SESSION['id']))
+    /*if (!isset($_SESSION['id']))
     {
         exit ("Error: Not logged in!");
     }
-    $id = $_SESSION["id"];
+    $id = $_SESSION["id"];*/
 
-    //$id = 'A0BwIAdiU9';
+    $id = 'A0BwIAdiU9';
 
     $city = htmlspecialchars($_GET['city']);
     $country = htmlspecialchars($_GET['country']);
