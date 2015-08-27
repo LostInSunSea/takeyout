781a12efc7bc1305a3ce63ffcda2822bf57f622e
etienne212 <austinsunc@yahoo.com>
2015-08-27 10:44:08 -0700

    "conversations added"

---
+++ www/php/getConversations.php
@@ -0,0 +1,32 @@
+<?php
+
+$ID = htmlspecialchars($_GET['userID']);
+
+$dbHost = 'localhost';
+$dbUser = "root";
+$dbPass = "J^mpStrt";
+$dbDatabase = "takeyout";
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM conversation WHERE user1 = '$ID' OR user2 = '$ID'";
+if ($result=mysqli_query($conn,$sql))
+{
+    if (mysqli_num_rows($result))
+    {		
+        $jsonstring = json_encode($result);
+        echo $jsonstring;
+    }
+    else
+    {
+        echo '{}';
+    }
+
+}
+$conn->close();
+
+?>
