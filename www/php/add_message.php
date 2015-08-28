62d324597d55be13a1af5406f54c871773d81da3
etienne212 <austinsunc@yahoo.com>
2015-08-28 15:04:59 -0700

    "oops"

--- www/php/add_message.php
+++ www/php/add_message.php
@@ -33,7 +33,7 @@
 //TODO: Add time
 
     $sql = "INSERT INTO reply (id, message, fromUser, toUser, time, conversationId)
-            VALUES (NULL, '."$message."', '$from', '$to', $time, '$convoID')";
+            VALUES (NULL, '".$message."', '$from', '$to', $time, '$convoID')";
 
     if ($conn->query($sql) === TRUE)
     {
