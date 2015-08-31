730ce83c7a90540d5410cce0753fd9f4f8a7a2b1
Yue Zhao <yuz217@ucsd.edu>
2015-08-31 11:26:31 -0700

    request meeting will create a meeting request object

---
+++ www/php/request_meeting.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['id']))
+{
+    exit ("Error: Not logged in!");
+}
+
+$dbHost = 'localhost';
+$dbUser = "root";
+$dbPass = "J^mpStrt";
+$dbDatabase = "takeyout";
+
+$id = $_SESSION['id'];
+
+$friend = $_POST['userId'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$place = $_POST['place'];
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO meetingRequest (id, time, date, place, sendId, receiveId)
+        VALUES (NULL, '$time', '$date', '$place', '$id', '$friend')";
+
+if (mysqli_query($conn, $sql))
+{
+    echo "Success";
+}
+else
+{
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+$conn->close();
+
+?>
