9a355a5963c31eae0c0cdd5ec48e1489df39e291
Yue Zhao <yuz217@ucsd.edu>
2015-08-30 18:04:52 -0700

    bug fix

--- www/php/get_friends.php
+++ www/php/get_friends.php
@@ -24,7 +24,24 @@
 
     $json = array();
 
-    $sql = "SELECT conversation.tripId, conversation.user1, conversation.user2, user.id, user.name, user.picFull, user.headline FROM conversation INNER JOIN user ON (conversation.user1 = user.id OR conversation.user2 = user.id) WHERE conversation.tripId = '$trip' AND (conversation.user1 = '$id' OR conversation.user2 = '$id')";
+    if ($trip == 0)
+    {
+        $selfSQL = "SELECT * FROM user WHERE id = '$id'";
+        if ($self = mysqli_query($conn,$selfSQL))
+        {
+            while ($row = mysqli_fetch_array($self))
+            {
+                $homeCity = $row['city'];
+                $homeCountry = $row['country'];
+            }
+
+            $sql = "SELECT conversation.tripId, conversation.user1, conversation.user2, user.id, user.name, user.picFull, user.headline FROM conversation INNER JOIN user ON (conversation.user1 = user.id OR conversation.user2 = user.id) WHERE conversation.city = '$homeCity' AND conversation.country = '$homeCountry' AND (conversation.user1 = '$id' OR conversation.user2 = '$id')";
+        }
+    }
+    else
+    {
+        $sql = "SELECT conversation.tripId, conversation.user1, conversation.user2, user.id, user.name, user.picFull, user.headline FROM conversation INNER JOIN user ON (conversation.user1 = user.id OR conversation.user2 = user.id) WHERE conversation.tripId = '$trip' AND (conversation.user1 = '$id' OR conversation.user2 = '$id')";
+    }
     if ($result=mysqli_query($conn,$sql))
     {
         while($row = mysqli_fetch_array($result))
