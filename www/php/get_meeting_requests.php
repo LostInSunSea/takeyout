336105b7cbe28c57bc0cc33af71e2680e3411076
Yue Zhao <yuz217@ucsd.edu>
2015-08-31 11:58:02 -0700

    get meeting requests returns the meeting requests with user data attached

---
+++ www/php/get_meeting_requests.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+if (!isset($_SESSION['id']))
+{
+    exit ("Error: Not logged in!");
+}
+
+$dbHost = 'localhost';
+$dbUser = "root";
+$dbPass = "J^mpStrt";
+$dbDatabase = "takeyout";
+
+$id = $_SESSION['id'];
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM meetingRequest INNER JOIN user ON meetingRequest.sentId = user.id WHERE receiveId = '$id'";
+
+$json = array();
+
+if ($result=mysqli_query($conn,$sql)) {
+    while ($row = mysqli_fetch_array($result))
+    {
+        $bus = array(
+            'name' => $row['name'],
+            'picFull' => $row['picFull'],
+            'date' => $row['date'],
+            'time' => $row['time'],
+            'place' => $row['place']
+        );
+
+        array_push($json, $bus);
+    }
+}
+
+$jsonstring = json_encode($json);
+echo $jsonstring;
+
+$conn -> close();
+
+?>
