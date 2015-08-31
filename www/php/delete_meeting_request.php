031986d3ed8f40cd14b90fad1d7c44a4150a4ff9
Yue Zhao <yuz217@ucsd.edu>
2015-08-31 13:52:11 -0700

    deletes a meeting request

---
+++ www/php/delete_meeting_request.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['id']))
+{
+    exit ("Error: Not logged in!");
+}
+
+$dbHost = 'localhost';
+$dbUser = "root";
+$dbPass = "J^mpStrt";
+$dbDatabase = "takeyout";
+
+$id = $_SESSION['id'];
+
+$requestId = $_POST['requestId'];
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "DELETE FROM meetingRequest WHERE receiveId = '$id' AND id = '$requestId'";
+
+if (mysqli_query($conn, $sql))
+{
+    echo "Success";
+}
+else
+{
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+$conn->close();
+
+
+?>
