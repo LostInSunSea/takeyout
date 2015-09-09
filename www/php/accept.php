4d6734b91e076a00e2b4f1c3b91f47d68bb3cc1a
Yue Zhao <yuz217@ucsd.edu>
2015-09-08 17:59:21 -0700

    no more time

--- www/php/accept.php
+++ www/php/accept.php
@@ -44,8 +44,7 @@
         if ($result->num_rows)
         {
             //TODO: delete this item, create a new conversation, and add to the reject table
-            $makeConversationSQL = "INSERT INTO conversation(id, time, user1, user2, tripId, city, country)
-                                    VALUES (NULL,NULL,'$id','$otherUser',$trip,'$city','$country')";
+            $makeConversationSQL = "INSERT INTO conversation(id, user1, user2, tripId, city, country) VALUES (NULL,'$id','$otherUser',$trip,'$city','$country')";
             echo $makeConversationSQL;
             if (mysqli_query($conn,$makeConversationSQL))
             {
