c8dfb90c4664c10f589b391c1abdf4e89433cbaf
Yue Zhao <yuz217@ucsd.edu>
2015-09-11 15:43:50 -0700

    and to hometown too, hopefully this works

--- www/php/match.php
+++ www/php/match.php
@@ -53,7 +53,7 @@
                 $city = $row['city'];
                 $country = $row['country'];
                 $finalSQL = "SELECT user.id, user.name, user.headline, user.industry, user.city, user.country, user.picFull, user.bio, user.languages, user.interests
-                                     FROM trip INNER JOIN user ON trip.owner = user.id WHERE user.id <> '$id' AND trip.city = '$city' AND trip.country = '$country' AND trip.active = 1;";
+                                     FROM trip INNER JOIN user ON trip.owner = user.id WHERE user.id <> '$id' AND trip.city = '$city' AND trip.country = '$country' AND trip.active = 1 AND user.id NOT IN (SELECT receiveId FROM rejection WHERE sentId = '$id');";
             }
         }
     }
