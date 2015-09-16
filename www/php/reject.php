763928d6d5b23550c927d5114bcbbb219c8275a6
Yue Zhao <yuz217@ucsd.edu>
2015-09-16 15:22:23 -0700

    reject will fill in trip id if started from a hometown

--- www/php/reject.php
+++ www/php/reject.php
@@ -14,6 +14,9 @@
     $id = $_SESSION['id'];
 
     $tripId = $_POST['trip'];
+    $otherUser = $_POST["otherUser"];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
 
     $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
     if ($conn->connect_error)
@@ -21,7 +24,18 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $otherUser = $_POST["otherUser"];
+    if ($tripId == 0)
+    {
+        $findTripSQL = "SELECT * FROM trip WHERE city = '$city' AND country = '$country' AND owner = '$otherUser'";
+        //TODO: check that it only returns 1 thing
+        if ($tripResult = mysqli_query($conn,$findTripSQL))
+        {
+            while ($row = mysqli_fetch_array($tripResult))
+            {
+                $tripId = $row['id'];
+            }
+        }
+    }
 
     $sql = "INSERT INTO rejection (id, sentId, receiveId, tripId) VALUES (NULL, '$id', '$otherUser', $tripId)";
 
