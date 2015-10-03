7653236464f2a9da1a553055bf421fe68eca80b0
Yue Zhao <yuz217@ucsd.edu>
2015-10-03 05:34:20 -0700

    hoho

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+$dbHost = '45.55.30.181';
+$dbUser = "root";
+$dbPass = "keyboard cat";
+$dbDatabase = "SDHacks2015";
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM stories";
+
+if ($result=mysqli_query($conn,$sql)) {
+    echo $result;
+}
+
+?>
